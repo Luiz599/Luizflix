d8ed2037519e6a06f4bc09c6977eea64f3799971
luiz599 <fernando2nik@gmail.com>
2025-03-27 22:19:09 -0300

    Feito até metade do slide 67

--- js/perfil.js
+++ js/perfil.js
@@ -1,6 +1,45 @@
+let perfis = [];
+let emEdicao = false;
+let perfilEdicao = null;
+
+function inicializarDados() {
+    let dados = JSON.parse(localStorage.getItem("luizflix-users"));
+    if (dados == null) {
+        dados = [
+            {
+                nome: "Luiz",
+                avatar: "img/users/avatar.png",
+                crianca: false
+            },
+            {
+                nome: "Capitão",
+                avatar: "img/users/avatar4.png",
+                crianca: false
+            },
+            {
+                nome: "Trizóio",
+                avatar: "img/users/avatar5.png",
+                crianca: true
+            }
+        ];
+        localStorage.setItem("luizflix-users", JSON.stringify(dados));
+    }
+    perfis = dados;
+}
+
 // Ao carregar a página executa as funções de buscar os dados
 document.addEventListener("DOMContentLoaded", async () => {
-    setTimeout(() => {
-        toggleLoading();
-    }, 2000);
+    carregarPerfis();
+    toggleLoading();
+    let nome = document.getElementById('Nome');
+    nome.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.querySelector('.btn-light').click();
+        }
+    });
+    let icons = document.querySelectorAll('.icon');
+    icons.forEach(icon => {
+        icon.computedStyleMap.display = 'none';
+    })
 });
