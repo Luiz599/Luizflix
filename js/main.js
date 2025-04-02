aab6f439a937573d24ed657b2627f805193f2b90
luiz599 <fernando2nik@gmail.com>
2025-04-02 20:46:37 -0300

    LuizFlix concluído!!!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// API key
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjc1ZDhiMGQ2YmM2N2E0OGIwOGFjMzc2MmY5ZDg2OSIsIm5iZiI6MTc0MzYyMTM5NS42MzIsInN1YiI6IjY3ZWQ4ZDEzODM2YzhlZGE3Y2FiMDc2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJO0TB-w17Yp1TZEvcNpQScwwMn6NoA5UIS3clojpLg'
+    }
+}
+
 // Exibir loading
 function toggleLoading() {
     let loader = document.querySelector(".loader");
